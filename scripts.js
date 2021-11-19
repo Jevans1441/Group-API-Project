f61e8e7430d56b7ef7a19af15424c166eca01c67
Josh Evans <88986229+Jevans1441@users.noreply.github.com>
2021-11-18 21:26:52 -0500

    updated event listener

--- scripts.js
+++ scripts.js
@@ -18,3 +18,32 @@ windyInit(options, (windyAPI) => {
   // .map is instance of Leaflet map
 });
 // Windy API End----------->
+
+// variables start
+const zipCode = document.querySelector("#zipCode");
+const btn = document.querySelector(".submit");
+//   variables end
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const userZip = zipCode.value;
+  const url = `http://api.openweathermap.org/data/2.5/weather?zip=${userZip},us&appid=710540d456e691680abdfc45926ba1b8&units=imperial`;
+  function fetchWeather() {
+    //   inputs API URL
+    fetch(url)
+      .then((Response) => Response.json())
+      .then((body) => {
+        //local variables
+        latitude = document.querySelector("#latitude");
+
+        let keys = Object.keys(body.main);
+        for (let i = 0; i < keys.length; i++) {
+          //   let key = keys[i];
+          // let value = body.main[key];
+          console.log(body);
+        }
+        latitude.innerHTML = body.coord.lat;
+      });
+  }
+  fetchWeather();
+});
